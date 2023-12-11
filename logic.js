3efb528d100531b9b671da7b548eb79776a9306d
busraates <busrates.89@gmail.com>
2023-12-11 17:07:28 +0000

    changes seect cars by colord

--- logic.js
+++ logic.js
@@ -240,7 +240,7 @@ async function selectCarsByColor(tx) {
     const carRegistry = await getAssetRegistry(namespace + '.Car');
 
     // Execute the query to select cars by color
-    const results = await query('selectCarsByColor');
+    const results = await query('SelectCarsByColor');
     console.log(results);
     for (let i = 0; i < results.length; i++) {
         let car = results[i];
